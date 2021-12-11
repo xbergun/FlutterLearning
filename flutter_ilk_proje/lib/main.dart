cc94bc577d6e98277345b154ef7bb9861b7a9d94
Berkay <berkay.ergun16@gmail.com>
2021-12-11 18:01:27 +0100

    11.12.2021 18.01
    11.12.2021 18.01

--- flutter_ilk_proje/lib/main.dart
+++ flutter_ilk_proje/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
@@ -9,12 +11,31 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
+        theme:
+            ThemeData(primaryColor: Colors.green, accentColor: Colors.purple),
         home: Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.amber,
-        title: Text('Başlık'),
-      ),
-      body: Text('Merhaba Dünya'),
-    ));
+            backgroundColor: Colors.yellow,
+            appBar: AppBar(
+              title: const Text('Selam'),
+            ),
+            body: Container(
+              //alignment kullandığımızda, içindeki veri kadar değil, parenti kadar yer kaplıyor.
+              color: Colors.purple,
+              child: Text(
+                'Berkay' * 2,
+                textAlign: TextAlign.center,
+              ),
+              constraints: BoxConstraints(minHeight: 100, minWidth: 100),
+            ),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () {
+                debugPrint("Tıklandı");
+              },
+              child: const Icon(
+                Icons.account_circle_rounded,
+                color: Colors.blueGrey,
+              ),
+              backgroundColor: Colors.red,
+            )));
   }
 }
